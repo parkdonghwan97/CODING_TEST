1ba8fbfd1e2b9ac6296084baea9e5c28643823cf
pdh <venus6941@gmail.com>
2022-04-06 01:15:32 +0900

    DFS올림피아드문제

--- 인프런/AA.py
+++ 인프런/AA.py
@@ -9,41 +9,35 @@
 
 
 
-
-# BFS
-from collections import deque
-
-
+def DFS(x,y,h):
+    ch[x][y]= 1
+    for i in range(4):
+        X = x+dx[i]
+        Y = y+dy[i]
+        if 0<=X<n and 0<=Y<n and ch[X][Y]==0 and board[X][Y] > h:
+            DFS(X,Y,h)
 n = int(input())
-board = [list(map(int,input().split() ))  for i in range(n)]
+board = [list(map(int,input().split()))for i in range(n) ]
+answer = 0
+cnt=0           # 이거 설정 안하면 4번 5번 안되더라구요  ??
+dx = [0,1,0,-1]
+dy = [-1,0,1,0]
+for x in range(100):
+    ch = [ [0]*n for i in range(n) ]  # 0  초기화
+    cnt =0
+    for i in range(n):
+        for j in range(n):
+            #0~100 사이 h값 보다 높은경우
+            if ch[i][j]==0 and board[i][j]> x:
+                cnt+=1
+                DFS(i,j,x)
+    answer = max(answer,cnt)
+    # cut 해줘야댐 4,5 번 오류
 
-# 상하좌우 아닌 팔각임
-# dx = [0,1,0,-1]
-# dy = [-1,0,1,0]
+    if cnt ==0:
 
-# 12시 기준 팔방 [북 북동 동 남동 남 남서 서 북서]
-dx = [0 ,1 ,1,1,0,-1,-1,-1]
-dy = [-1,-1,0,1,1,1,0,-1]
-
-dq = deque()
-answer = []
-for i in range(n):
-    for j in range(n):
-        if board[i][j]== 1:
-            board[i][j] = 0
-            dq.append([i,j]  )
-            while dq:
-                
-                cnt = 0
-                tmp  = dq.popleft()
-                for a in range(8):
-                    X = dx[a] + tmp[0]
-                    Y = dy[a] + tmp[1]
-                    if 0<=X<n and 0<=Y<n and board[X][Y] ==1:
-                        board[X][Y] = 0 
-                        dq.append([X,Y])
-                        cnt+=1
-            answer.append(cnt)        
-print(len(answer))
+        break
 
 
+
+print(answer)
